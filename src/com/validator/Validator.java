7b082fac4456b8bea91535ef3d9d07d5e65ace24
Carlo <carlo.regidor22@gmail.com>
2017-11-05 20:52:24 +0800

    Validator Interface Document

--- src/com/validator/Validator.java
+++ src/com/validator/Validator.java
@@ -1,8 +1,26 @@
+/*
+ * 
+ */
 package com.validator;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Validator.
+ */
 public interface Validator {
 
+	/**
+	 * Validate.
+	 *
+	 * @return true, if successful
+	 */
 	public boolean validate();
+	
+	/**
+	 * Assert validate.
+	 *
+	 * @return true, if successful
+	 */
 	public boolean assertValidate();
 	
 }
